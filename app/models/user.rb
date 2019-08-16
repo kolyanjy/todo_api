b66a594a973a93ff3fa484a24801424bba2aa25a
kolyanjy <kolyan654d@gmail.com>
2019-08-16 14:32:10 +0300

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A\S+@.+\.\S+\z/
+
   has_secure_password
 
-  validates :email, uniqueness: true, format: /@/
+
+  validates :email, uniqueness: true, format: VALID_EMAIL_REGEX
   validates :email, :password, presence: true, length: { in: 5..50 }
 end
